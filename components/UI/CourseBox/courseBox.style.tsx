f47a8d1cc1e5608c082dd8195e5ec405b4539723
ahmadshoaib-git <ahmad.shoaib88@gmail.com>
2023-02-11 04:35:58 +0500

    feat: Course Box component updated.

--- components/UI/CourseBox/courseBox.style.tsx
+++ components/UI/CourseBox/courseBox.style.tsx
@@ -1,5 +1,5 @@
 const styles = {
-    courseContainer: 'h-[6rem] w-[20rem] lg:w-[25rem] bg-dark-sea-blue flex rounded-lg',
+    courseContainer: 'h-[6rem] w-[16rem] md:[20rem] lg:w-[25rem] bg-dark-sea-blue flex rounded-lg',
 };
 
 export { styles };
