47b0a5b8bc0cf0a927f3e70235a7f0524badc44f
ahmadshoaib-git <ahmad.shoaib88@gmail.com>
2023-02-11 04:35:19 +0500

    feat: Tabs component added.

---
+++ components/UI/Tabs/index.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React from 'react';
+import { Tabs, Tab } from './tabs';
+
+interface Props {
+    tabData: Array<{ title: string; children: React.ReactElement }>;
+}
+
+const CustomTabs = ({ tabData }: Props) => {
+    const [active, setActive] = React.useState<number>(0);
+
+    const handleChange = (newActive: React.SetStateAction<number>) => setActive(newActive);
+    return (
+        <div className="w-full">
+            <Tabs active={active} onChange={handleChange}>
+                {tabData.map((nTab, index) => (
+                    <Tab key={`T-${index.toString()}`} title={nTab.title}>
+                        {nTab.children}
+                    </Tab>
+                ))}
+            </Tabs>
+        </div>
+    );
+};
+
+export default CustomTabs;
+
---
+++ components/UI/Tabs/tabs.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface PropsTabs {
+    active: number;
+    onChange: (newActive: React.SetStateAction<number>) => void;
+    children: any;
+}
+
+const Tab = ({ children }: any) => <div>{children}</div>;
+
+const tabActiveClass = 'pointer font-semibold border-b-4 border-green !text-green !bg-black';
+
+const Tabs = ({ active, onChange, children }: PropsTabs) => {
+    return (
+        <>
+            <div className="flex justify-center gap-[2rem]">
+                {children.map((c: { props: { title: any } }, index: number) => (
+                    <a
+                        href={'javascript: void(0)'}
+                        onClick={() => onChange(index)}
+                        // className={active === index ? 'tab activeTab' : 'tab '}
+                        className={`text-center uppercase tab text-2xl font-Barlow px-[1rem] py-[0.5rem] mb-[3rem] hover:text-white ${
+                            active === index && tabActiveClass
+                        }`}
+                    >
+                        {c.props.title}
+                    </a>
+                ))}
+            </div>
+            <div>{children[active]}</div>
+        </>
+    );
+};
+
+export { Tabs, Tab };
+
