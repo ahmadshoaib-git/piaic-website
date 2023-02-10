bc745f8179de9ed6eada30fb306be9fc24278071
ahmadshoaib-git <ahmad.shoaib88@gmail.com>
2023-02-11 04:33:08 +0500

    feat: Metaverse section added.

--- components/Sections/Metaverse/index.tsx
+++ components/Sections/Metaverse/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+// import { Timeline, TimelineEvent } from '@mailtop/horizontal-timeline';
+// import { FaBug, FaRegCalendarCheck, FaRegFileAlt } from 'react-icons/fa';
 import TitleBar from '../TitleBar';
 import ReadMore from '@/components/UI/ReadMore';
 
@@ -7,9 +9,9 @@ const Metaverse = () => {
     return (
         <TitleBar title="Metaverse & Web 3.0" position={'left'}>
             <>
-                <div className="w-full text-white flex gap-8 flex-col-reverse lg:flex-row">
+                <div className="group w-full text-white flex gap-8 flex-col-reverse lg:flex-row">
                     <div className="flex-3 flex flex-col gap-8 items-start justify-center mt-[2rem]">
-                        <div className="text-gray family-Roboto font-normal text-2xl text-justify">
+                        <div className="text-gray family-Roboto font-normal text-2xl text-justify mb-[1rem]">
                             <p className="mb-[2rem]">
                                 A communal environment or virtual world made possible by the fusion of physical and virtual reality is referred to as the
                                 "metaverse." Users can engage in shared interactions with a wide range of virtual objects and environments in this imagined
@@ -23,7 +25,12 @@ const Metaverse = () => {
                         <ReadMore title="Read More" href="#" />
                     </div>
                     <div className="flex-1 flex justify-center items-center lg:justify-center">
-                        <Image src="/images/metaverse.png" alt="Metaverse" fill className="max-w-[20rem] min-w-[20rem] max-h-[20rem] min-h-[20rem] !static" />
+                        <Image
+                            src="/images/metaverse.png"
+                            alt="Metaverse"
+                            fill
+                            className="max-w-[20rem] min-w-[20rem] max-h-[20rem] min-h-[20rem] !static group-hover:sectionImageAnimation"
+                        />
                     </div>
                 </div>
             </>
